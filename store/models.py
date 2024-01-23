e0730ca4294a659d0b9cdb3bab274e92f7290aa3
Rohit Singh <rohit224455@gmail.com>
2024-01-24 00:52:31 +0530

    store and carts funtionality

--- store/models.py
+++ store/models.py
@@ -18,4 +18,33 @@ class Product(models.Model):
         return reverse('products_detail', args=[self.category.slug, self.slug])
 
     def __str__(self):
+
         return self.product_name
+
+
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category="color", is_active=True)
+    
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active=True)
+    
+
+variation_category_choice=(
+    ('color','color'),
+    ('size','size')
+)
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    vairation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_date = models.DateTimeField(auto_now=True)
+
+    objects =VariationManager()
+
+    def __str__(self):
+        return self.vairation_value
+
+
